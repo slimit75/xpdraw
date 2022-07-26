145c8e2fe251459929fe64481b07a24f84e2a087
slimit75 <limitspeed75@gmail.com>
2022-07-26 16:40:02 -0400

    Actually restructure some functions

--- src/xpdraw.cpp
+++ src/xpdraw.cpp
@@ -1,6 +1,7 @@
 #include "xpdraw.h"
 
 #include <filesystem>
+
 #if LIN
 	#include <GL/gl.h>
 #elif __GNUC__
@@ -8,6 +9,7 @@
 #else
 	#include <GL/gl.h>
 #endif
+
 #include <XPLMPlugin.h>
 #include <XPLMUtilities.h>
 #include <math.h>
@@ -27,33 +29,124 @@ int anchor_x = 0;
 int anchor_y = 0;
 int lastRight = 0;
 int lastGlyphWidth = 0;
-sd_texture charBitmapCache[256][256];
+xpdraw::texture charBitmapCache[256][256];
 
-FT_Library ft;
+//FT_Library ft;
 
-string findPluginPath() {
-    XPLMEnableFeature("XPLM_USE_NATIVE_PATHS", 1);
+namespace xpdraw {
+    namespace tools {
+        string findPluginPath() {
+            XPLMEnableFeature("XPLM_USE_NATIVE_PATHS", 1);
 
-    char pluginPathChar[256];
-    XPLMGetPluginInfo(XPLMGetMyID(), NULL, pluginPathChar, NULL, NULL);
-    string pluginPath = pluginPathChar;
-    pluginPath.erase(pluginPath.end() - 10, pluginPath.end());
+            char pluginPathChar[256];
+            XPLMGetPluginInfo(XPLMGetMyID(), NULL, pluginPathChar, NULL, NULL);
+            string pluginPath = pluginPathChar;
+            pluginPath.erase(pluginPath.end() - 10, pluginPath.end());
 
-    return pluginPath;
-}
+            return pluginPath;
+        }
 
-string findXPlanePath() {
-    XPLMEnableFeature("XPLM_USE_NATIVE_PATHS", 1);
+        string findXPlanePath() {
+            XPLMEnableFeature("XPLM_USE_NATIVE_PATHS", 1);
 
-    char xplanePath[512];
-    XPLMGetSystemPath(xplanePath);
-    string xpPath = xplanePath;
+            char xplanePath[512];
+            XPLMGetSystemPath(xplanePath);
+            string xpPath = xplanePath;
+            
+            return xpPath;
+        }
+    }
     
-    return xpPath;
-}
+    /*
+    namespace fonts {
+        void initFT() {
+            if (FT_Init_FreeType(&ft)) {
+                logMsg("Error initializing FreeType library!");
+            }
+        }
 
-namespace simdraw {
-    sd_texture loadBuffer(void* buffer, int width, int height, GLenum format) {
+        void cacheChar(FT_Face face, int size, char character) {
+            FT_Set_Pixel_Sizes(face, 0, size * 1.5);
+            FT_GlyphSlot g = face->glyph;
+            FT_Load_Char(face, character, FT_LOAD_RENDER);
+            charBitmapCache[size][character - '0'] = loadBuffer(g->bitmap.buffer, g->bitmap.width, g->bitmap.rows, GL_ALPHA);
+        }
+
+        void loadFont(FT_Face *font, string filename) {
+            // Get file locations
+            string filePath = filename;
+
+            if(FT_New_Face(ft, filePath.c_str(), 0, font)) {
+                logMsg("Error loading font!");
+            }
+        }
+
+        void drawText(FT_Face face, string textString, float x, float y, int size, int align, xpdraw::color color = { 1, 1, 1, 1 }) {
+            glColor4f(color.red, color.green, color.blue, color.alpha);
+
+            const char* text = textString.c_str();
+            FT_Set_Pixel_Sizes(face, 0, size * 1.5);
+            const char *p;
+            FT_GlyphSlot g = face->glyph;
+            float width = 0;
+
+            // Attempt to calculate the length of the string *without* drawing it
+            for(p = text; *p; p++) {
+                // Cache char data if it isn't already
+                if (!charBitmapCache[size][p[0] - '0'].gl_texture) {
+                    cacheChar(face, size, p[0]);
+                }
+
+                // Windows is completely stupid and does NOT load the metrics when told to load METRICS ONLY!!!!!!!
+                if (isspace(p[0])) {
+                    width = width + lastGlyphWidth + 1.3;
+                }
+                else {
+                    lastGlyphWidth = charBitmapCache[size][p[0] - '0'].width;
+                    width = width + lastGlyphWidth + 1.3;
+                }
+            }
+            width = width - 1.3;
+
+            if (align == ALIGN_CENTER) {
+                x = x - (width / 2);
+            }
+            else if (align == ALIGN_RIGHT) {
+                x = x - width;
+            }
+
+            // Render text using cached data
+            for(p = text; *p; p++) {
+                int y_offset;
+                if (p[0] == 'Q') {
+                    y_offset = size / -6.5;
+                }
+                else if (p[0] == '-') {
+                    y_offset = size / 3;
+                }
+                else if (p[0] == '.') {
+                    x += 3;
+                    width += 6;
+                }
+                else {
+                    y_offset = 0;
+                }
+
+                if (isspace(p[0])) {
+                    x = lastRight + lastGlyphWidth + 2.6;
+                }
+                else {
+                    xpdraw::texture image = charBitmapCache[size][p[0] - '0'];
+                    simdraw::drawFlippedTexture(image, x, y + y_offset, image.width, image.height, color);
+                    lastGlyphWidth = image.width;
+                    x = lastRight + 1.3;
+                }
+            }
+        }
+    }
+    */
+
+    xpdraw::texture loadBuffer(void* buffer, int width, int height, GLenum format) {
         GLuint tex;
         glGenTextures(1, &tex);
         glBindTexture(GL_TEXTURE_2D, tex);
@@ -62,7 +155,7 @@ namespace simdraw {
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
         glBindTexture(GL_TEXTURE_2D, 0);
 
-        sd_texture return_value;
+        xpdraw::texture return_value;
         return_value.gl_texture = tex;
         return_value.width = width;
         return_value.height = height;
@@ -70,25 +163,12 @@ namespace simdraw {
         return return_value;
     }
 
-    void initFT() {
-        if (FT_Init_FreeType(&ft)) {
-            logMsg("Error initializing FreeType library!");
-        }
-    }
-
-    void cacheChar(FT_Face face, int size, char character) {
-        FT_Set_Pixel_Sizes(face, 0, size * 1.5);
-        FT_GlyphSlot g = face->glyph;
-        FT_Load_Char(face, character, FT_LOAD_RENDER);
-        charBitmapCache[size][character - '0'] = loadBuffer(g->bitmap.buffer, g->bitmap.width, g->bitmap.rows, GL_ALPHA);
-    }
-
     void setAnchor(int newAnchor_x, int newAnchor_y) {
         anchor_x = newAnchor_x;
         anchor_y = newAnchor_y;
     }
     
-    void drawRect(int left, int bottom, int width, int height, sd_color color) {
+    void drawRect(int left, int bottom, int width, int height, xpdraw::color color = { 1, 1, 1, 1 }) {
         glColor4f(color.red, color.green, color.blue, color.alpha);
 
         int x1 = anchor_x + left;
@@ -107,7 +187,7 @@ namespace simdraw {
         glEnd();
     }
 
-    void drawQuad(int x1, int y1, int x2, int y2, int x3, int y3, int x4, int y4, sd_color color) {
+    void drawQuad(int x1, int y1, int x2, int y2, int x3, int y3, int x4, int y4, xpdraw::color color = { 1, 1, 1, 1 }) {
         glColor4f(color.red, color.green, color.blue, color.alpha);
 
         x1 += anchor_x;
@@ -130,7 +210,7 @@ namespace simdraw {
         glEnd();
     }
 
-    void drawRectBorder(int left, int bottom, int width, int height, sd_color color, int borderSize, sd_color borderColor) {
+    void drawRectBorder(int left, int bottom, int width, int height, xpdraw::color color = { 1, 1, 1, 1 }, int borderSize, xpdraw::color borderColor = { 1, 1, 1, 1 }) {
         drawRect(left, bottom, width, height, color);
         drawRect(left, bottom, borderSize, height, borderColor);
         drawRect(left + width - borderSize, bottom, borderSize, height, borderColor);
@@ -138,7 +218,7 @@ namespace simdraw {
         drawRect(left, bottom, width, borderSize, borderColor);
     }
 
-    void drawLine(int start_x, int start_y, int end_x, int end_y, sd_color color) {
+    void drawLine(int start_x, int start_y, int end_x, int end_y, xpdraw::color color = { 1, 1, 1, 1 }) {
         glColor4f(color.red, color.green, color.blue, color.alpha);
 
         int x1 = start_x + anchor_x;
@@ -151,7 +231,7 @@ namespace simdraw {
         glEnd();
     }
 
-    void drawLine(int start_x, int start_y, int end_x, int end_y, sd_color color, int thickness) {
+    void drawLine(int start_x, int start_y, int end_x, int end_y, xpdraw::color color = { 1, 1, 1, 1 }, int thickness) {
         glColor4f(color.red, color.green, color.blue, color.alpha);
 
         int x1 = start_x + anchor_x;
@@ -166,7 +246,33 @@ namespace simdraw {
         glEnd();
     }
 
-    void drawTexture(sd_texture texture, int left, int bottom, int width, int height, sd_color color) {
+    void drawTexture(xpdraw::texture texture, int left, int bottom, xpdraw::color color = { 1, 1, 1, 1 }) {
+        glColor4f(color.red, color.green, color.blue, color.alpha);
+
+        int x1 = anchor_x + left;
+        int y1 = anchor_y + bottom;
+        int x2 = x1 + texture.width;
+        int y2 = y1 + texture.height;
+
+        glBindTexture(GL_TEXTURE_2D, texture.gl_texture);
+        glEnable(GL_TEXTURE_2D);
+        glBegin(GL_QUADS);
+        glTexCoord2f(0, 0);
+        glVertex2f(x1, y1);
+        glTexCoord2f(0, 1);
+        glVertex2f(x1, y2);
+        glTexCoord2f(1, 1);
+        glVertex2f(x2, y2);
+        glTexCoord2f(1, 0);
+        glVertex2f(x2, y1);
+        glEnd();
+        glDisable(GL_TEXTURE_2D);
+        glBindTexture(GL_TEXTURE_2D, 0);
+
+        lastRight = x2 - anchor_x;
+    }
+
+    void drawTexture(xpdraw::texture texture, int left, int bottom, int width, int height, xpdraw::color color = { 1, 1, 1, 1 }) {
         glColor4f(color.red, color.green, color.blue, color.alpha);
 
         int x1 = anchor_x + left;
@@ -192,7 +298,7 @@ namespace simdraw {
         lastRight = x2 - anchor_x;
     }
 
-    void drawFlippedTexture(sd_texture texture, int left, int bottom, int width, int height, sd_color color) {
+    void drawFlippedTexture(xpdraw::texture texture, int left, int bottom, int width, int height, xpdraw::color color = { 1, 1, 1, 1 }) {
         glColor4f(color.red, color.green, color.blue, color.alpha);
 
         int x1 = anchor_x + left;
@@ -218,7 +324,7 @@ namespace simdraw {
         lastRight = x2 - anchor_x;
     }
 
-    void drawRotatedTexture(sd_texture texture, float angle, float left, float bottom, float width, float height, float rx, float ry, sd_color color) {
+    void drawRotatedTexture(xpdraw::texture texture, float angle, float left, float bottom, float width, float height, float rx, float ry, xpdraw::color color = { 1, 1, 1, 1 }) {
         glColor4f(color.red, color.green, color.blue, color.alpha);
 
         rx = left + rx;
@@ -256,100 +362,25 @@ namespace simdraw {
         glBindTexture(GL_TEXTURE_2D, 0);
     }
 
-    sd_texture loadTexture(string filename) {
-        // We don't want the texture upside down!
+    xpdraw::texture loadTexture(string filename) {
         stbi_set_flip_vertically_on_load(true);
 
-        // Get file locations
-        string filePath = findPluginPath() += filename;
+        string filePath = xpdraw::tools::findPluginPath() += filename;
 
-        // Load the texture
         int width, height, nrChannels;
         unsigned char* texDat = stbi_load(filePath.c_str(), &width, &height, &nrChannels, 4);
 
         if (texDat == NULL) {
-            logMsg("Failed to load texture: %s", filePath.c_str());
+            throw("Failed to load texture: %s!", filePath.c_str());
 
             return { 0, 0, 0 };
         }
         
         // Return texture
-        sd_texture return_value;
+        xpdraw::texture return_value;
         return_value = loadBuffer(texDat, width, height, GL_RGBA);
         stbi_image_free(texDat);
 
         return return_value;
-    }
-
-    void loadFont(FT_Face *font, string filename) {
-        // Get file locations
-        string filePath = filename;
-
-        if(FT_New_Face(ft, filePath.c_str(), 0, font)) {
-            logMsg("Error loading font!");
-        }
-    }
-
-    void drawText(FT_Face face, string textString, float x, float y, int size, int align, sd_color color) {
-        glColor4f(color.red, color.green, color.blue, color.alpha);
-
-        const char* text = textString.c_str();
-        FT_Set_Pixel_Sizes(face, 0, size * 1.5);
-        const char *p;
-        FT_GlyphSlot g = face->glyph;
-        float width = 0;
-
-        // Attempt to calculate the length of the string *without* drawing it
-        for(p = text; *p; p++) {
-            // Cache char data if it isn't already
-            if (!charBitmapCache[size][p[0] - '0'].gl_texture) {
-                cacheChar(face, size, p[0]);
-            }
-
-            // Windows is completely stupid and does NOT load the metrics when told to load METRICS ONLY!!!!!!!
-            if (isspace(p[0])) {
-                width = width + lastGlyphWidth + 1.3;
-            }
-            else {
-                lastGlyphWidth = charBitmapCache[size][p[0] - '0'].width;
-                width = width + lastGlyphWidth + 1.3;
-            }
-        }
-        width = width - 1.3;
-
-        if (align == ALIGN_CENTER) {
-            x = x - (width / 2);
-        }
-        else if (align == ALIGN_RIGHT) {
-            x = x - width;
-        }
-
-        // Render text using cached data
-        for(p = text; *p; p++) {
-            int y_offset;
-            if (p[0] == 'Q') {
-                y_offset = size / -6.5;
-            }
-            else if (p[0] == '-') {
-                y_offset = size / 3;
-            }
-            else if (p[0] == '.') {
-                x += 3;
-                width += 6;
-            }
-            else {
-                y_offset = 0;
-            }
-
-            if (isspace(p[0])) {
-                x = lastRight + lastGlyphWidth + 2.6;
-            }
-            else {
-                sd_texture image = charBitmapCache[size][p[0] - '0'];
-                simdraw::drawFlippedTexture(image, x, y + y_offset, image.width, image.height, color);
-                lastGlyphWidth = image.width;
-                x = lastRight + 1.3;
-            }
-        }
     }
 }
--- src/xpdraw.h
+++ src/xpdraw.h
@@ -9,6 +9,7 @@
 #endif
 
 #include <string>
+#include <vector>
 
 /*
     Drawing functions to simplify OpenGL
@@ -18,34 +19,46 @@
 #define ALIGN_CENTER 2
 #define ALIGN_RIGHT 3
 
-struct sd_texture {
-    GLuint gl_texture;
-    int width;
-    int height;
-};
+namespace xpdraw {
+    struct texture {
+        GLuint gl_texture;
+        int width;
+        int height;
+    };
 
-struct sd_color {
-    float red;
-    float green;
-    float blue;
-    float alpha;
-};
+    struct color {
+        float red;
+        float green;
+        float blue;
+        float alpha = 1;
+    };
 
-std::string findPluginPath();
-std::string findXPlanePath();
+    struct font {
+        texture cache[256][256];
+    };
 
-namespace simdraw {
-    void initFT();
+    namespace tools {
+        std::string findPluginPath();
+        std::string findXPlanePath();
+    }
+
+    /*
+    namespace fonts {
+        void initFonts();
+        void loadFont(FT_Face *font, std::string filename);
+        void drawText(FT_Face face, std::string textString, float x, float y, int size, int align, xpdraw::color color);
+    }
+    */
+
+    xpdraw::texture loadBuffer(void* buffer, int width, int height, GLenum format);
     void setAnchor(int newAnchor_x, int newAnchor_y);
-    void drawRect(int left, int bottom, int width, int height, sd_color color);
-    void drawRectBorder(int left, int bottom, int width, int height, sd_color color, int borderSize, sd_color borderColor);
-    void drawQuad(int x1, int y1, int x2, int y2, int x3, int y3, int x4, int y4, sd_color color);
-    void drawLine(int start_x, int start_y, int end_x, int end_y, sd_color color);
-    void drawLine(int start_x, int start_y, int end_x, int end_y, sd_color color, int thickness);
-    void drawTexture(sd_texture texture, int left, int bottom, int width, int height, sd_color color);
-    void drawRotatedTexture(sd_texture texture, float angle, float left, float bottom, float width, float height, float rx, float ry, sd_color color);
-    sd_texture loadTexture(std::string filename);
-    void loadFont(FT_Face *font, std::string filename);
-    void drawText(FT_Face face, std::string textString, float x, float y, int size, int align, sd_color color);
-    sd_texture loadBuffer(void* buffer, int width, int height, GLenum format);
+    void drawRect(int left, int bottom, int width, int height, xpdraw::color color = { 1, 1, 1, 1 });
+    void drawRectBorder(int left, int bottom, int width, int height, xpdraw::color color = { 1, 1, 1, 1 }, int borderSize, xpdraw::color borderColor = { 1, 1, 1, 1 });
+    void drawQuad(int x1, int y1, int x2, int y2, int x3, int y3, int x4, int y4, xpdraw::color color = { 1, 1, 1, 1 });
+    void drawLine(int start_x, int start_y, int end_x, int end_y, xpdraw::color color = { 1, 1, 1, 1 });
+    void drawLine(int start_x, int start_y, int end_x, int end_y, xpdraw::color color = { 1, 1, 1, 1 }, int thickness);
+    void drawTexture(xpdraw::texture texture, int left, int bottom, xpdraw::color color = { 1, 1, 1, 1 });
+    void drawTexture(xpdraw::texture texture, int left, int bottom, int width, int height, xpdraw::color color = { 1, 1, 1, 1 });
+    void drawRotatedTexture(xpdraw::texture texture, float angle, float left, float bottom, float width, float height, float rx, float ry, xpdraw::color color = { 1, 1, 1, 1 });
+    xpdraw::texture loadTexture(std::string filename);
 }
