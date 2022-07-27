8449617f7576a36bd25f6fce1c8becfa8d772485
slimit75 <limitspeed75@gmail.com>
2022-07-27 14:21:12 -0400

    Fix OGL include in windows.cpp

--- src/windows.cpp
+++ src/windows.cpp
@@ -1,6 +1,14 @@
 #include <XPLMDisplay.h>
 #include <XPLMGraphics.h>
-#include <GL/gl.h>
+
+#if LIN or __MINGW32__
+	#include <GL/gl.h>
+#elif __GNUC__
+	#include <OpenGL/gl.h>
+#else
+	#include <GL/gl.h>
+#endif
+
 #include "xpdraw/windows.h"
 
 using namespace xpdraw;
