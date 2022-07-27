2472e445160e3a5c20b2a4e8cceeeac9d3a9aa9c
slimit75 <limitspeed75@gmail.com>
2022-07-27 11:29:02 -0400

    Restructure file locations

---
+++ src/tools.cpp
@@ -0,0 +1,30 @@
+#include <string>
+#include <XPLMPlugin.h>
+#include <XPLMUtilities.h>
+
+#include "xpdraw/tools.h"
+
+using namespace std;
+
+namespace tools {
+    string findPluginPath() {
+        XPLMEnableFeature("XPLM_USE_NATIVE_PATHS", 1);
+
+        char pluginPathChar[256];
+        XPLMGetPluginInfo(XPLMGetMyID(), NULL, pluginPathChar, NULL, NULL);
+        string pluginPath = pluginPathChar;
+        pluginPath.erase(pluginPath.end() - 10, pluginPath.end());
+
+        return pluginPath;
+    }
+
+    string findXPlanePath() {
+        XPLMEnableFeature("XPLM_USE_NATIVE_PATHS", 1);
+
+        char xplanePath[512];
+        XPLMGetSystemPath(xplanePath);
+        string xpPath = xplanePath;
+        
+        return xpPath;
+    }
+}
