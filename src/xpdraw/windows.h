93d8188f1a818dcdde849b0e066e18396de285b7
slimit75 <limitspeed75@gmail.com>
2022-07-27 12:42:50 -0400

    Add inital windows function

--- src/xpdraw/windows.h
+++ src/xpdraw/windows.h
@@ -1,7 +1,26 @@
 #pragma once
 
-namespace xpdraw::windows {
-    class window {
+#include <XPLMDisplay.h>
+#include <string>
 
+namespace xpdraw::windows {
+    class Window {
+        public:
+            XPLMCreateWindow_t params;
+            XPLMWindowID windowID;
+            int texNum;
+            unsigned char* buffer;
+            int width;
+            int height;
+
+            XPLMDrawWindow_f drawFunc;
+            XPLMHandleMouseClick_f mouseFunc;
+            XPLMHandleMouseWheel_f wheelFunc;
+            XPLMHandleKey_f keyFunc;
+            XPLMHandleCursor_f cursorFunc;
+
+            void initWindow(int newWidth, int newHeight);
+            void setDrawFunc(XPLMDrawWindow_f newDrawFunc);
+            void createWindow(std::string title, int winLeft = 50, int winDown = 150, int minScale = 1, int maxScale = 1);
     };
 }
