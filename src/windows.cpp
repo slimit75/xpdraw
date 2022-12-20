adab474b72175774b2cf0a18ee9ce755e12b3df8
slimit75 <slimit75@pm.me>
2022-12-20 11:50:07 -0500

    Use pointers to access windows

--- src/windows.cpp
+++ src/windows.cpp
@@ -11,40 +11,42 @@
 
 #include "xpdraw/windows.h"
 
-namespace xpdraw {
-    void Window::initWindow(int newWidth, int newHeight) {
-        buffer = new unsigned char[newWidth * newHeight * 4];
-        width = newWidth;
-        height = newHeight;
+namespace xpdraw::windows {
+    void newWindow(window* inWindow, int width, int height) {
+        inWindow->width = width;
+        inWindow->height = height;
+        inWindow->buffer = new unsigned char[width * height * 4];
+
+        inWindow->params.structSize = sizeof(inWindow->params);
+        inWindow->params.visible = 1;
+        inWindow->params.refcon = NULL;
+        inWindow->params.layer = xplm_WindowLayerFloatingWindows;
+        inWindow->params.decorateAsFloatingWindow = xplm_WindowDecorationRoundRectangle;
     }
 
-    void Window::createWindow(std::string title, int winLeft, int winDown) {
-        params.structSize = sizeof(params);
-        params.visible = 1;
-        params.drawWindowFunc = drawFunc;
-        params.handleMouseClickFunc = mouseFunc;
-        params.handleRightClickFunc = mouseFunc;
-        params.handleMouseWheelFunc = wheelFunc;
-        params.handleKeyFunc = keyFunc;
-        params.handleCursorFunc = cursorFunc;
-        params.refcon = NULL;
-        params.layer = xplm_WindowLayerFloatingWindows;
-        params.decorateAsFloatingWindow = xplm_WindowDecorationRoundRectangle;
+    void setCallbacks(window* inWindow, XPLMHandleMouseClick_f mouseHandler, XPLMDrawWindow_f drawHandler, XPLMHandleMouseWheel_f mouseWheelHandler, XPLMHandleKey_f keyHandler, XPLMHandleCursor_f cursorHandler) {
+      inWindow->params.drawWindowFunc = drawHandler;
+      inWindow->params.handleMouseClickFunc = mouseHandler;
+      inWindow->params.handleRightClickFunc = mouseHandler;
+      inWindow->params.handleMouseWheelFunc = mouseWheelHandler;
+      inWindow->params.handleKeyFunc = keyHandler;
+      inWindow->params.handleCursorFunc = cursorHandler;
+    }
 
+    void createWindow(window* inWindow, std::string title, int winLeft, int winDown) {
         int left, bottom, right, top;
         XPLMGetScreenBoundsGlobal(&left, &top, &right, &bottom);
-        params.left = left + winLeft;
-        params.bottom = bottom + winDown;
-        params.right = params.left + width - 20;
-        params.top = params.bottom + height - 20;
+        inWindow->params.left = left + winLeft;
+        inWindow->params.bottom = bottom + winDown;
+        inWindow->params.right = inWindow->params.left + inWindow->width - 20;
+        inWindow->params.top = inWindow->params.bottom + inWindow->height - 20;
+        inWindow->windowID = XPLMCreateWindowEx(&inWindow->params);
         
-        windowID = XPLMCreateWindowEx(&params);
-        
-        XPLMSetWindowPositioningMode(windowID, xplm_WindowPositionFree, -1);
-        XPLMSetWindowTitle(windowID, title.c_str());
+        XPLMSetWindowPositioningMode(inWindow->windowID, xplm_WindowPositionFree, -1);
+        XPLMSetWindowTitle(inWindow->windowID, title.c_str());
     }
 
-    void Window::setResizeLimits(int minWidth, int minHeight, int maxWidth, int maxHeight) {
-        XPLMSetWindowResizingLimits(windowID, minWidth - 20, minHeight - 20, maxWidth - 20, maxHeight - 20);
+    void setResizeLimits(window* inWindow, int minWidth, int minHeight, int maxWidth, int maxHeight) {
+        XPLMSetWindowResizingLimits(inWindow->windowID, minWidth - 20, minHeight - 20, maxWidth - 20, maxHeight - 20);
     }
 }
