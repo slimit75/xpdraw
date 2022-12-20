e9ff5f6ba3f67849d250ab82d3ae45d24ac8a175
slimit75 <slimit75@pm.me>
2022-12-20 11:50:07 -0500

    Use pointers to access windows

--- src/xpdraw/windows.h
+++ src/xpdraw/windows.h
@@ -4,73 +4,21 @@
 #include <XPLMDisplay.h>
 #include <string>
 
-namespace xpdraw {
-    class Window {
-        public:
-            XPLMCreateWindow_t params;
-            XPLMWindowID windowID;
-            int texNum;
-            unsigned char* buffer;
-            int width;
-            int height;
-
-            /**
-             * @brief Draw function that is called to render the window. Define this!!!
-             * 
-             */
-            XPLMDrawWindow_f drawFunc;
-
-            /**
-             * @brief Function to handle mouse clicks; if you need this, make sure to define it
-             * 
-             */
-            XPLMHandleMouseClick_f mouseFunc;
-
-            /**
-             * @brief Function to handle scroll wheels; if you need this, make sure to define it
-             * 
-             */
-            XPLMHandleMouseWheel_f wheelFunc;
-
-            /**
-             * @brief Function to handle key presses; if you need this, make sure to define it
-             * 
-             */
-            XPLMHandleKey_f keyFunc;
-
-            /**
-             * @brief Function to handle mouse cursors; if you need this, make sure to define it
-             * 
-             */
-            XPLMHandleCursor_f cursorFunc;
-
-            /**
-             * @brief Creates a new window with the specified width and height
-             * 
-             * @param newWidth Width of new window
-             * @param newHeight Height of new window
-             */
-            void initWindow(int newWidth, int newHeight);
-
-            /**
-             * @brief Starts drawing a window with the specified title.
-             * 
-             * @param title Title of window
-             * @param winLeft X location of window relative to X-Plane window; defaults to 50
-             * @param winDown Y location of window relative to X-PLane window; defaults to 150
-             */
-            void createWindow(std::string title, int winLeft = 50, int winDown = 150);
-
-            /**
-             * @brief Define resize limits for the newly created window
-             * 
-             * @param minWidth Minimum width of window
-             * @param minHeight Minimum height of window
-             * @param maxWidth Maximum width of window
-             * @param maxHeight Maximum height of window
-             */
-            void setResizeLimits(int minWidth, int minHeight, int maxWidth, int maxHeight);
+namespace xpdraw::windows {
+    struct window {
+        XPLMCreateWindow_t params;
+        XPLMWindowID windowID;
+        int texNum;
+        unsigned char* buffer;
+        int width;
+        int height;
     };
+
+    // TODO: Documentation
+    void newWindow(window* inWindow, int width, int height);
+    void setCallbacks(window* inWindow, XPLMHandleMouseClick_f mouseHandler, XPLMDrawWindow_f drawHandler, XPLMHandleMouseWheel_f mouseWheelHandler, XPLMHandleKey_f keyHandler, XPLMHandleCursor_f cursorHandler);
+    void createWindow(window* inWindow, std::string title, int winLeft = 50, int winDown = 150);
+    void setResizeLimits(window* inWindow, int minWidth, int minHeight, int maxWidth, int maxHeight);
 }
 
 #endif
