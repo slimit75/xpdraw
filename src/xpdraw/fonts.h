a9fc25512ec68ce8dbb2ef33a71a1b9eff135db6
slimit75 <hi@slimit75.me>
2024-05-02 15:29:19 -0400

    refactor: convert remaining stuff to C

--- src/xpdraw/fonts.h
+++ src/xpdraw/fonts.h
@@ -77,7 +77,7 @@ int xpd_text_length(xpd_font_face_t *font, const char *text, int size);
  * @param color Color of the text; defaults to white
  */
 void xpd_text_draw(xpd_font_face_t *font, const char *text, int x, int y, int size, xpd_text_align_t align,
-                   xpd_color_t color);
+				   xpd_color_t color);
 
 #ifdef __cplusplus
 }
